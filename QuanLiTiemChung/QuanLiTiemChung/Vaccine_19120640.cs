3306f9b8176daaba9c252a52bbddf54a51a04b45
Mean-Sun <tranminhsonyi@gmail.com>
2022-07-05 13:25:30 +0700

    merge from main, fix small async

--- QuanLiTiemChung/QuanLiTiemChung/Vaccine_19120640.cs
+++ QuanLiTiemChung/QuanLiTiemChung/Vaccine_19120640.cs
@@ -13,9 +13,9 @@ namespace QuanLiTiemChung
         public string NSX;
         public DateTime HSD;
         Vaccine_DB_19120640 vaccine_DB = new Vaccine_DB_19120640();
-        public static void Lay_thong_tin_Vaccine(Dictionary<string, string> list_VX)
+        public static Dictionary<string, string> Lay_thong_tin_Vaccine()
         {
-            list_VX = Vaccine_DB_19120640.getAllvaccine();
+            return Vaccine_DB_19120640.getAllvaccine();
         }
     }
 }
